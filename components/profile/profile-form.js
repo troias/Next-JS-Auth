bc8c4f8ca3b5155f7d4d51de842708c3bd8c6d20
Troy Flavell <hunter007nz@gmail.com>
2021-10-26 20:56:29 +1300

    Added front end API connection

--- components/profile/profile-form.js
+++ components/profile/profile-form.js
@@ -1,15 +1,39 @@
 import classes from './profile-form.module.css';
+import {useRef } from 'react'
 
 function ProfileForm() {
+
+  const oldPasswordRef = useRef();
+  const newsPasswordRef = useRef();
+
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    const entOldPass = event.target.value;
+    const entNewPassword = event.target.value
+
+    const enteredPassworsd = {
+      oldPassword: entOldPass,
+      newPassword: entNewPassword
+    }
+    const req = await fetch('/api/user/change-password', {
+      method: 'PATCH',  
+      body: JSON.stringify(enteredPassworsd), 
+      contentType: 'application/json'
+    })
+
+    const res = await req.json()
+    console.log("res", res)
+  }
+
   return (
-    <form className={classes.form}>
+    <form onSubmit={submitHandler} className={classes.form}>
       <div className={classes.control}>
         <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <input type='password' id='new-password' ref={newsPasswordRef} />
       </div>
       <div className={classes.control}>
         <label htmlFor='old-password'>Old Password</label>
-        <input type='password' id='old-password' />
+        <input type='password' id='old-password'  ref={oldPasswordRef} />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
--- pages/api/user/change-password.js
+++ pages/api/user/change-password.js
@@ -1,6 +1,8 @@
-import {getSession} from 'next-auth/client'
+import { getSession } from 'next-auth/client'
+import { connectToDB } from '../../../helpers/db'
+import { comparePassword, hashpassword} from '../../../helpers/auth'
 
-const handler = async (req,res) => {
+const handler = async (req, res) => {
 
     if (!req.method === "PATCH") {
         return
@@ -11,14 +13,50 @@ const handler = async (req,res) => {
     })
 
     if (!session) {
-        res.status(401).json({message: "Auth failed"})
-        return 
+        res.status(401).json({ message: "Auth failed" })
+        return
     }
 
     if (session) {
 
     }
+    const newPass = req.body.newPassword
+    const oldPass = req.body.oldPassword
+    const useremail = session.user.email
 
+    const client = await connectToDB()
+
+    const userCollection = await client.db().collection('users')
+    const user = await userCollection.findOne({ email: useremail })
+
+    if (!user) {
+        res.status(404).json({ message: "Failed to find user"})
+        client.close()
+        return
+    }
+
+    const currPass = user.hashedPassword
+
+    const isValid = await comparePassword(oldPass, currPass )
+
+    if (!isValid) {
+        res.status(403).json({ message: "Password does not match"})
+        client.close()
+        return
+    }
+
+    const hashedPassword = await hashpassword(newPass)
+
+    const result = await userCollection.updateOne({
+        email: useremail
+    }, {
+        $set: {
+            hashedPassword: hashedPassword
+        }
+    })
+
+    client.close()
+    res.status(200).json({ message: "Password updated"})
 }
 
 export default handler;
